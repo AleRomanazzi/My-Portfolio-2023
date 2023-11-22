f312f37da36bac85de392215684463b78d0d62b8
Alejandro <romanazzialejandro2014@gmail.com>
2023-11-21 21:06:15 -0300

    creación y diseño de sección contacto

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,16 +1,53 @@
 import React from "react";
-import InputContact from "../_input";
+import ThemeContext from "../_themeContext";
+import SwitchTheme from "../_switchTheme";
+import { useState } from 'react'
+import InputContact from '../_input'
 import Button from "../_button";
 
 const Contact = () => {
+
+    const [Theme, setTheme] = useState('dark')
+
     return(
-        <div>
-            <InputContact campo={"Nombre"} type={"text"} />
-            <InputContact campo={"Apellido"} type={"text"} />
-            <InputContact campo={"Email"} type={"e-mail"} />
-            <textarea name="Mensaje" cols="30" rows="10"></textarea>
-            <Button text={"Enviar Mensaje"} />
-        </div>
+        <ThemeContext.Provider value={Theme}>
+            <div className={`container ${Theme}` }>
+                <form style={{
+                    padding: '4rem 25rem',
+                    border: '2px solid rgba(84, 60, 90, 1)',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} action="" method="post">
+                    <div style={{ display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'}}>
+                        <h1 style={{fontFamily: '"Josefin Sans"',color:"white"}}>CONTACTO</h1>
+                        <InputContact type={"text"} content={"Nombre"} />
+                        <InputContact type={"text"} content={"Apellido"} />
+                        <InputContact type={"e-mail"} content={"Email"} />
+                        <textarea 
+                        style={{
+                            fontFamily: '"Baloo 2"',
+                            padding: "2px",
+                            backgroundColor: "transparent",
+                            color: "white",
+                            margin: "1rem 0",
+                            border: '1px solid rgba(84, 60, 90, 1)',
+                            width: "25rem",
+                            height: "10rem"}} 
+                            name="Mensaje" 
+                            placeholder="Escriba su mensaje"></textarea>
+                        <Button text={"Enviar Mensaje"} /> 
+                    </div>        
+                </form>
+                <div>
+                    
+                </div>
+            </div>
+        </ThemeContext.Provider>
     )
 }
 
