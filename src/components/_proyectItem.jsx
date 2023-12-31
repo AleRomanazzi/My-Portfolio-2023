3cc3b7f3a2da0d867096efd7d0e682c05bd70230
Alejandro Romanazzi <romanazzialejandromain@gmail.com>
2023-12-31 13:12:18 -0300

    agregado de seccion sobre mi con el tema oscuro y el boton volver

--- src/components/_proyectItem.jsx
+++ src/components/_proyectItem.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const ProyectItem = ({ srcImage, title, description, proyectLink }) => {
+const ProyectItem = ({ srcImage, title, description, proyectLink, link }) => {
     return(
             <div className="proyectDiv">
                 <img className="img" src={srcImage}></img>
                 <div className="proyectText">
-                    <div className="proyectTitle">{title}</div>
+                    <div className="proyectTitle"><b>{title}</b></div>
                     <div className="description">{description}</div>
                     <a className="github" href={`${proyectLink}`} target="_blank">
-                        <img src="../src/assets/github.png" alt="" />
+                        <img src={`../src/assets/icons/${link}.png`} alt="" />
                     </a>
                 </div>
             </div>
--- src/components/pages/Aboutme.jsx
+++ src/components/pages/Aboutme.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ThemeContext from "../_themeContext";
 import { useState } from "react";
-
+import { Link } from "react-router-dom";
 const Aboutme = () =>{
     const [Theme, setTheme] = useState('dark')
 
@@ -9,6 +9,12 @@ const Aboutme = () =>{
         <ThemeContext.Provider value={Theme}>
             <div className={`container ${Theme}`}>
                 <h1>AboutMe</h1>
+                <div className="backPage">
+                    <Link to='/' className="linkBack">
+                        <img className="imgBackPage" src='../src/assets/flecha.png'/>
+                        <p>Volver</p>
+                    </Link>
+                </div>
             </div>
         </ThemeContext.Provider>
     )
